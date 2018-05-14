2681f38a1025106d09287df623032a1ef9a75a50
Horiuchi Fubuki <fubuki199914@gmail.com>
2018-05-14 17:05:00 +0900

    二回目のコミット

---
+++ Unity Test Project/Assets/Test2.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 5;
+        array[2] = 10;
+        array[3] = 15;
+        array[4] = 3;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        int[] point = { 83, 15, 91, 93, 55 };
+        for (int i = 0; i < point.Length; i++)
+        {
+            if (point[i] > 90)
+                Debug.Log(point[i]);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Unity Test Project/Assets/Test3.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player
+{
+    public void Attack()
+    {
+
+    }
+}
+
+public class Test3 : MonoBehaviour {
+
+    int add(int a, int b)
+    {
+        int c;
+        c = a + b;
+
+        return c;
+    }
+    void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+    void CallName(string name)
+    {
+        Debug.Log("Hello" + name);
+    }
+
+	// Use this for initialization
+	void Start () {
+        SayHello();
+        CallName("Horiuchi");
+        int ans;
+        ans = add(2, 3);
+        Debug.Log(ans);
+
+        Player myplayer = new Player();
+        myplayer.Attack();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
